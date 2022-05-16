3f25803e6c5bf85f04c9d9354bbb1fd18c8cc23d
Jonasthj <jonasthenderupjensen@gmail.com>
2022-05-16 15:52:31 +0200

    Initial Commit

---
+++ src/main/kotlin/no/kristiania/pro201examserver/Pro201ExamServerApplication.kt
@@ -0,0 +1,11 @@
+package no.kristiania.pro201examserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Pro201ExamServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<Pro201ExamServerApplication>(*args)
+}
---
+++ src/test/kotlin/no/kristiania/pro201examserver/Pro201ExamServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.kristiania.pro201examserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Pro201ExamServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
