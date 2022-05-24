33ab0ff93958ded76131f30cc6f85003f6da8f15
Jonasthj <jonasthenderupjensen@gmail.com>
2022-05-24 13:21:40 +0200

    more quiz info

--- src/main/resources/db/migration/V13__init_quiz_data.sql
+++ src/main/resources/db/migration/V13__init_quiz_data.sql
@@ -4,14 +4,20 @@ values (1),
        (3);
 
 insert into questions
-values (1, 'What is 2 + 2?', 1),
-       (2, 'Where is Norway?', 1),
-       (3, 'How far is one mile?', 2);
+values (1, 'Hva er 2 + 2?', 1),
+       (2, 'Hvor er Norge?', 1),
+       (3, 'Hvor lang er en mil?', 2);
 
 insert into answers
 values (1, '3', false, 1),
        (2, '2', false, 1),
        (3, '4', true, 1),
-       (4, 'In Africa', false, 2),
-       (5, 'In Europe', true, 2),
-       (6, 'In Asia', false, 2);
+       (4, '5', false, 1),
+       (5, 'I Afrika', false, 2),
+       (6, 'I Europa', true, 2),
+       (7, 'I Oceania', false, 2),
+       (8, 'I Asia', false, 2),
+       (9, '1km', false, 3),
+       (10, '2km', false, 3),
+       (11, '3km', false, 3),
+       (12, '1.6km', true, 3);
