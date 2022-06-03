5e02ee10ab138ee9cc64aa511a7f6f656d563247
Jonasthj <jonasthenderupjensen@gmail.com>
2022-06-03 12:37:59 +0200

    cross origin

--- src/main/kotlin/no/kristiania/pro201examserver/controller/QuizController.kt
+++ src/main/kotlin/no/kristiania/pro201examserver/controller/QuizController.kt
@@ -4,15 +4,13 @@ import no.kristiania.pro201examserver.model.quiz.QuizEntity
 import no.kristiania.pro201examserver.services.QuizService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/quiz")
 class QuizController(@Autowired private val quizService: QuizService) {
 
+    @CrossOrigin
     @GetMapping
     fun getQuiz(@RequestParam quizId: Long): ResponseEntity<QuizEntity> {
         return ResponseEntity.ok().body(quizService.getQuiz(quizId))
