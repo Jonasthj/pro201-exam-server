6bd08c12f97006f09059d0aabb2218a3d402bf2c
Martin Teigen <martin@teigen.biz>
2022-06-07 15:17:11 +0200

    Update info

--- src/main/resources/db/migration/V70__init_minigame_data.sql
+++ src/main/resources/db/migration/V70__init_minigame_data.sql
@@ -8,9 +8,30 @@ insert into minigame_rounds
 values (1, 1, 1, 1, 1),
        (2, 2, 1, 1, 1),
        (3, 3, 1, 1, 1),
+
        (4, 1, 1, 1, 2),
        (5, 2, 1, 1, 2),
-       (6, 3, 1, 1, 2);
+       (6, 3, 1, 1, 2),
+
+       (7, 1, 1, 1, 3),
+       (8, 2, 1, 1, 3),
+       (9, 3, 1, 1, 3),
+
+       (10, 1, 1, 1, 4),
+       (11, 2, 1, 1, 4),
+       (12, 3, 1, 1, 4),
+
+       (13, 1, 1, 1, 5),
+       (14, 2, 1, 1, 5),
+       (15, 3, 1, 1, 5),
+
+       (16, 1, 1, 1, 6),
+       (17, 2, 1, 1, 6),
+       (18, 3, 1, 1, 6),
+
+       (19, 1, 1, 1, 7),
+       (20, 2, 1, 1, 7),
+       (21, 3, 1, 1, 7);
 
 insert into minigame_scores
 values (nextval('minigame_scores_score_id_seq'), 2, 34.2, now(), 1, 1),
@@ -38,4 +59,54 @@ values (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01',
        (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 5),
        (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 6),
        (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 6),
-       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 6);
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 6),
+
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 7),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 7),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 7),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 8),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 8),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 8),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 9),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 9),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 9),
+
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 10),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 10),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 10),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 11),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 11),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 11),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 12),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 12),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 12),
+
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 13),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 13),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 13),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 14),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 14),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 14),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 15),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 15),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 15),
+
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 16),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 16),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 16),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 17),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 17),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 17),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 18),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 18),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 18),
+
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 19),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 19),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 19),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 20),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 20),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 20),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_01', 21),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_02', 21),
+       (nextval('memorycards_memory_id_seq'), 'MiniGameSprites/Memory/image_03', 21);
