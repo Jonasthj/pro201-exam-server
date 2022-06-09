b143047b129cb43030e9d9d24dc542679361af2d
Martin Teigen <martin@teigen.biz>
2022-06-09 15:19:49 +0200

    Add transactional to delete method

--- src/main/kotlin/no/kristiania/pro201examserver/repo/quiz/QuizRepo.kt
+++ src/main/kotlin/no/kristiania/pro201examserver/repo/quiz/QuizRepo.kt
@@ -3,9 +3,13 @@ package no.kristiania.pro201examserver.repo.quiz
 import no.kristiania.pro201examserver.model.quiz.QuizEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import javax.transaction.Transactional
 
 @Repository
 interface QuizRepo : JpaRepository<QuizEntity, Long> {
+
     fun findQuizEntityById(quizId: Long): QuizEntity?
+
+    @Transactional
     fun deleteAllById(id: Long?)
 }
